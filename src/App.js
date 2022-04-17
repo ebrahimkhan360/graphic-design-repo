8248ce4a0447005ed53abc5b557e0ce2191c95ca
ebrahimkhan360 <md.ebrahim.khan.w@gmail.com>
2022-04-17 15:10:09 +0600

    Services add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import Banner from './Pages/Home/Banner/Banner';
+import { Route, Routes } from 'react-router-dom';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Banner></Banner>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            
+            <h2>Footr</h2>
         </footer>
     );
 };
