88efa4a01b8ea6c02747102667117ef247d78481
ebrahimkhan360 <md.ebrahim.khan.w@gmail.com>
2022-04-18 12:01:57 +0600

    login

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Banner></Banner>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId'element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path="/about" element={<About></About>}></Route>
       </Routes>
       <Footer></Footer>
